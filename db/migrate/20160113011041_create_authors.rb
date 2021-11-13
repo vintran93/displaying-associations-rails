d87df4ab28b44d2c9718bcd1fa0ad85ac6e4a5e9
vincent <vincentuong@yahoo.com>
2021-11-12 23:33:51 -0600

    commit tree c634706b62677fac4684ba4f3122879e0e90dde3

--- db/migrate/20160113011041_create_authors.rb
+++ db/migrate/20160113011041_create_authors.rb
@@ -1,11 +1,11 @@
-class CreateAuthors < ActiveRecord::Migration
-  def change
-    create_table :authors do |t|
-      t.string :name
-      t.string :genre
-      t.text :bio
+#class CreateAuthors < ActiveRecord::Migration
+#  def change
+    #create_table :authors do |t|
+#      t.string :name
+#      t.string :genre
+#      t.text :bio
 
-      t.timestamps null: false
-    end
-  end
-end
+#      t.timestamps null: false
+#    end
+#  end
+#end
